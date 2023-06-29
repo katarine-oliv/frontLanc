3bbefac228c0ee1e07f59d850aa40f68b28b345e
[kath] <[katarineoliveira41@gmail.com]>
2023-06-29 11:33:53 -0300

    segundo commit

--- src/app/principal/pedido/pedido.ts
+++ src/app/principal/pedido/pedido.ts
@@ -2,15 +2,22 @@ import { Cliente } from "../cliente/cliente";
 import { Produto } from "../produto/produto";
 
 export class Pedido{
-    id: number | undefined;
-    quantidade!: number | undefined;
-    cliente!: Cliente | undefined;
-    produto!: Produto | undefined;
+    id: number | undefined
+    quantidade!: number | undefined
+    observacao!: string | undefined
+    total!: boolean | undefined
+    data!: Date | undefined
+    cliente!: Cliente | undefined
+    produto!: Produto | undefined
 
-    constructor(id?: number, quantidade?: number, cliente?: Cliente, produto?: Produto){
-        this.id = id;
-        this.quantidade = quantidade;
-        this.cliente = cliente;
-        this.produto = produto;
-    }
+
+  constructor(id?: number, quantidade?: number, observacao?: string, total?: boolean, data?: Date, cliente?: Cliente, produto?: Produto) {
+    this.id = id;
+    this.quantidade = quantidade;
+    this.observacao = observacao;
+    this.total = total;
+    this.data = data;
+    this.cliente = cliente;
+    this.produto = produto;
+  }
 }
